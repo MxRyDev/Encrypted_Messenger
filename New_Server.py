317770503c6dd4b343c9ffa13ff96361e1418228
Royce Pope <roycepope@royce-mbp.oakgatetech.com>
2014-03-14 11:31:48 -0700

    Created a shutdown Sequence
    Trying to figure out how to gracefully shutdown everything

--- New_Server.py
+++ New_Server.py
@@ -1,13 +1,12 @@
 from threading import Thread
 import socket
-import time
-import sys
+import time, sys, os
 
 
 # initialize Variables:
 
 HOST = ''
-PORT = 5006
+PORT = 5007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
 print ('====================SOCKET CREATED====================\n')
 
@@ -42,7 +41,39 @@ print ('              >----------[ ]----------<')
 time.sleep(.3)
 print ('              >----------[=]----------<\n')
 time.sleep(.5)
-print ('=================Socket Bind Complete=================\n')
+print ('==================TCP LOCK INITIATED==================\n')
+
+def spinner(delay):
+    for char in '/-\|':        
+      sys.stdout.write("                           "+char)
+      sys.stdout.flush()
+      sys.stdout.write("\b" * 28)
+      time.sleep(delay)
+
+print ("                   'ENTER'to lock\n")
+lock = 1
+spinner(.3)
+lock = input()
+lock = 2
+spinner(.3)
+lock = input()
+lock = 3
+spinner(.3)
+lock = input()
+lock = 4
+spinner(.3)
+lock = input()
+lock = 5
+if lock == 5:
+    for x in range(0,10):
+        spinner(.05)
+    print ("                          \ /\n")
+    #print ("========================LOCKED========================\n")
+    print ("                    ##############")
+    print ("                    ####LOCKED####")
+    print ("                    ##############")
+time.sleep(1)    
+print ('==================Port Bind Complete==================\n')
 time.sleep(1)
 print ("          ===Starting Listening Sequence===\n")
 s.listen(5)
@@ -59,8 +90,8 @@ def listening(s):
             i = 0
             listening(s)
             
-listening_thread = Thread(target=listening(s))
-listening_thread.start()
+#listening_thread = Thread(target=listening(s))
+#listening_thread.start()
 
 
 # Need to convert this to Python3            
@@ -76,7 +107,7 @@ def client_connect():
     while True:
         print ("Socket Listening...")
         conn, addr = s.accept()
-        print('Connected by', addr)
+        print('Connected by', addr[0] + ':' + str(addr[1]))
         clients.append(conn)
         
         
@@ -98,25 +129,70 @@ def msg_snd_rcv():
             for message in msg:
                 msg_snd(message)
     
-
+    
 
 accept_connections = Thread(target = client_connect)
 snd_and_rcv = Thread(target =  msg_snd_rcv)
+time.sleep(1)
+print('\n')
 
 accept_connections.start()
 print('accepting connections...')
+time.sleep(1)
+print('\n')
 
 snd_and_rcv.start()
-print('handling messages...')
+print('handling messages...\n')
+time.sleep(1)
 
-
-print('Server fully operational')
+print('====================SERVER ONLINE====================')
 
 while True:
     admin = input('>>')
     
-    if admin == 'close':
-        quit()
+    if (admin == 'close') or (admin == 'exit'):
+        print ('==============STARTING SHUTDOWN SEQUENCE=============')
+        if clients != []:
+            s.shutdown(1)
+        time.sleep(1)
+        print ('DISENGAGING TCP LOCK...\n')
+
+        time.sleep(1)
+        print ('UN-BINDING PORTS...\n')
+        print ('              >----------[=]----------<')
+        time.sleep(.1)
+        print ('              >----------[ ]----------<')
+        time.sleep(.1)
+        print ('              >---------[   ]---------<')
+        time.sleep(.1)
+        print ('              >--------[     ]--------<')
+        time.sleep(.1)
+        print ('              >-------[       ]-------<')
+        time.sleep(.1)
+        print ('              >------[         ]------<')
+        time.sleep(.1)
+        print ('              >-----[           ]-----<')
+        time.sleep(.1)
+        print ('              >----[             ]----<')
+        time.sleep(.1)
+        print ('              >---[               ]---<')
+        time.sleep(.1)
+        print ('              >--[                 ]--<')
+        time.sleep(.1)
+        print ('              >-[                   ]-<')
+      
+        time.sleep(1)
+        print ('PERFORMING CLEANUP PROCESS...\n')
+        # Clearing lists
+        clients = []
+        message_queue = []
+        time.sleep(1)
+        print ('CLOSING SOCKET...\n')
+        s.close()
+        time.sleep(1)
+        print ('goodbye.')
+        
+        os._exit(1)
         
         
 
